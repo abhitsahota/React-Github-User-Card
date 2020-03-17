3a4be865ccde080b9a658ea71451de409d7fc1d7
abhitsahota <abhit.sahota@gmail.com>
2020-03-16 21:25:36 -0400

    init

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Card } from 'reactstrap';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <div className="App"></div>
+  constructor() {
+    super();
+    this.state = {};
+  } // Constructor end bracket
 
-  );
-}
+  componentDidMount() {
+
+    const getUsers = () => {
+      axios.get()
+        .then(users => this.setState({}))
+        .catch(err => {
+          console.log(err)
+        });
+  };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Github User</h1>
+
+      </div>
+  
+    ) // Return end parantheses
+  } // Render component end bracket
+} // App component end bracket
 
 export default App;
