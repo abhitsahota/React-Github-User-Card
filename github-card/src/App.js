407b95d18a1f8614097be39578071134aa502885
abhitsahota <abhit.sahota@gmail.com>
2020-03-17 21:51:32 -0400

    loaded date

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Card } from 'reactstrap';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 import axios from 'axios';
 
 import './App.css';
@@ -8,24 +11,43 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      username: 'abhitsahota',
+      name: '',
+      url: '',
+      profileImg: '',
+      bio: ''
+    };
   } // Constructor end bracket
 
   componentDidMount() {
-
-    const getUsers = () => {
-      axios.get()
-        .then(users => this.setState({}))
+      axios.get(`https://api.github.com/users/${this.state.username}`)
+        .then((res) => {
+          this.setState({
+            name: res.data.name, 
+            url: res.data.html_url,
+            profileImg: res.data.avatar_url,
+            bio: res.data.bio
+          })
+        })
         .catch(err => {
           console.log(err)
         });
-  };
-  }
+  } // CDU end bracket
 
   render() {
     return (
       <div>
         <h1>Github User</h1>
+        <Card>
+        <CardImg top width="50%" src={this.state.profileImg} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{this.state.name}</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
 
       </div>
   
